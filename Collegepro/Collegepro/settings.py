1e35ff190f2f91eb8e93e802c9e5c03db1ba548c
krish8924 <156788659+krish8924@users.noreply.github.com>
2026-02-17 18:33:07 +0530

    Fix ALLOWED_HOSTS format in settings.py

--- Collegepro/Collegepro/settings.py
+++ Collegepro/Collegepro/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^f^vz+r-*e9&y&+q)0il2dv@#snhu!5a%4a20x)jaw&y5dl6yn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
