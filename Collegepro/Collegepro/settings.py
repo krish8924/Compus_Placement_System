2837fa3f49a77f774c0c83a0192cfce88e04d5b0
krish8924 <156788659+krish8924@users.noreply.github.com>
2026-02-17 17:46:34 +0530

    Set DEBUG to False and configure ALLOWED_HOSTS
    Changed DEBUG setting to False and updated ALLOWED_HOSTS.

--- Collegepro/Collegepro/settings.py
+++ Collegepro/Collegepro/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^f^vz+r-*e9&y&+q)0il2dv@#snhu!5a%4a20x)jaw&y5dl6yn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
